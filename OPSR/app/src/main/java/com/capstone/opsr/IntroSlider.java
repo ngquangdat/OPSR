1b9e8cbb342003f4acb8a91d663ee70098def3c1
Scorpion1406 <31349402+Scorpion1406@users.noreply.github.com>
2019-05-15 09:41:31 -0700

    IntroSlider

---
+++ OPSR/app/src/main/java/com/capstone/opsr/IntroSlider.java
@@ -0,0 +1,13 @@
+package com.capstone.opsr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroSlider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_slider);
+    }
+}
