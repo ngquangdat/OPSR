7f1a72032a332da6bb37cbd7246652abbce98461
ngquangdat <ngquangdat13@gmail.com>
2019-05-15 20:47:26 +0700

    Initial commit

---
+++ app/src/main/java/com/capstone/opsr/MainActivity.java
@@ -0,0 +1,16 @@
+package com.capstone.opsr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+}
